ce6d94fb7b44607b92e7f6b6d9025f584e98ef51
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-01-23 22:25:17 -0300

    aplicando las renders pro para optimizar la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
 import { useState } from 'react'
 import './App.css'
-
-const getWeather = async (city = 'moscu') => {
-
-  try {
-    console.log((`${import.meta.env.VITE_APP_WEATHER_API}key=${import.meta.env.VITE_APP_WEATHER_KEY}&q=${city}`))
-    // http://api.weatherapi.com/v1/search.xml?key=<YOUR_API_KEY>&q=lond
-    const reqWeather = await fetch(`${import.meta.env.VITE_APP_WEATHER_API}key=${import.meta.env.VITE_APP_WEATHER_KEY}&q=${city}`)
-    const respWeather = await reqWeather.json();
-    console.log(respWeather);
-    return respWeather;
-  } catch (err) {
-    console.log('Imposible acceder al clima en estos momentos')
-    return err;
-  }
-}
+import useCityInfo from './component/handleWeather';
+import SearchCapital from './component/searchCapital';
 
 function App() {
-  const [capital, setCapital] = useState([])
-  // setCapital(getWeather);
-  return <>
-    <form className='formCity'>
-      <label> Ciudad:
-        <input type='text' className='inputText inputFormCity-text' placeholder='Capital  ej:"London"' />
-        <input type='submit' className='inputButton inputFormCity-button' value='buscar' />
-      </label>
-    </form>
 
+  const { capital, setCapital, handleCapital } = useCityInfo();
+
+  return <>
+    <SearchCapital handleCapital={handleCapital} />
     <hr />
-    {!!!capital &&
+    {(capital.length != 0) &&
       <div>
-        <div className='uiShowCapital'>Santiago</div>
+        <div className='uiShowCapital'></div>
+        <div className='uiShowCountry'></div>
         <div className='uiShowTemp'>28 °</div>
         <div className='uiShowIcon'>icono</div>
         <div className='Mapa'>Mapa</div>
       </div>
     }
-    {!!capital &&
-      <div className='uiShowNotice'>Escriba una capital por favor.</div>
+    {
+      (capital.length === 0) && <div className='uiShowNotice'>Escriba una capital por favor.</div>
     }
-
   </>
 }
 
